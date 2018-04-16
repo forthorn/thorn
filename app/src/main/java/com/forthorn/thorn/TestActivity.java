307684143a85b4a6c93d611c72f43b437d82990d
forthorn <yanpeng@huomaotv.com>
2018-04-16 19:42:42 +0800

    modify dependencies;

---
+++ app/src/main/java/com/forthorn/thorn/TestActivity.java
@@ -0,0 +1,13 @@
+package com.forthorn.thorn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_test);
+    }
+}
